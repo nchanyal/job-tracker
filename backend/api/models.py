9c8bfba1ea84587b5319b3e98f04a30005d68999
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-10-15 15:47:47 -0400

    Add CustomUser model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """Custom user model where email is used instead of username."""
+
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # This is handled by Django's AbstractBaseUser
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    # Use email as the unique identifier for authentication
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Fields required to create a superuser
+
+    def __str__(self):
+        """Return a string representation of the user."""
+        return self.email
