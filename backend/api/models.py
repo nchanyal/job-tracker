5adac0c35b26cc754be8a66a4930612ed16fddd0
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-10-17 17:19:20 -0400

    Create model JobApplication

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from .managers import CustomUserManager
 from django.core.validators import MinLengthValidator
+from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     """Custom user model where email is used instead of username."""
@@ -25,3 +26,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return a string representation of the user."""
         return self.email
+
+class JobApplication(models.Model):
+    class JobStatus(models.TextChoices):
+        APPLYING = 'Applying'
+        APPLIED = 'Applied'
+        INTERVIEWING = 'Interviewing'
+        INTERVIEWED = 'Interviewed'
+        OFFER = 'Offer'
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    job_title = models.CharField(max_length=30, blank=False, null=False)
+    company = models.CharField(max_length=20, blank=False, null=False)
+    job_location = models.CharField(max_length=20, blank=False, null=False)
+    application_status = models.CharField(max_length=12, choices=JobStatus, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.job_title} {self.company} {self.application_status}'
