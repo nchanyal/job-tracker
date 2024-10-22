a6350fb4ba53bc423c5b22d4d5a6a13c85442d32
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-10-22 13:36:02 -0400

    Remove second serializer for JobApplication

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -17,25 +17,9 @@ class CustomUserSerializer(serializers.ModelSerializer):
         return user
 
 class JobApplicationSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
     class Meta:
         model = JobApplication
         fields = ('id', 'job_title', 'company', 'job_location', 'application_status')
-
-class SecondJobApplicationSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = JobApplication
-        fields = ('id', 'job_title', 'company', 'job_location', 'application_status')
-
-    def validate_id(self, value):
-        try:
-            jobApplication = JobApplication.objects.get(id=value)
-            return value
-        except JobApplication.DoesNotExist:
-            raise serializers.ValidationError('Invalid id')
 
 class JobApplicationIdSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
