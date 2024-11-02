82f43c29468edcafd83fd65eed547f141cb3307b
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-11-02 13:07:57 -0400

    Handle input logic for SignInForm
    Users will be unable to type in the form fields
    without the handleChange function

--- src/components/SignInForm/index.jsx
+++ src/components/SignInForm/index.jsx
@@ -1,18 +1,35 @@
 import classes from './SignInForm.module.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function SignInForm() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        setFormData((previousFormData) => ({
+            ...previousFormData,
+            [name]: value,
+        }));
+
+        console.log(formData);
+    }
+
     return (
         <div className={`${classes.formContainer}`}>
             <p className={`${classes.signIn}`}>Sign in</p>
             <form action='#' method='post'>
                 <div className={`${classes.inputContainer}`}>
                     <p>Email</p>
-                    <input type='email' id='email' name='user_email' required/>
+                    <input type='email' id='email' name='email' onChange={handleChange} required/>
                 </div>
                 <div className={`${classes.inputContainer}`}>
                     <p>Password</p>
-                    <input type='password' id='password' name='user_password' required/>
+                    <input type='password' id='password' name='password' onChange={handleChange} required/>
                 </div>
                 <button className={`${classes.submitButton}`} type='submit'>Log In</button>
                 <p>No Account? <Link to='/register'>Create one.</Link></p>
