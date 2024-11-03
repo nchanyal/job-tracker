85d8057a19cc2b1907d24283b29773d2e5e86568
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-11-03 12:39:08 -0500

    Handle input logic for RegisterForm

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,26 +1,43 @@
 import classes from '../SignInForm/SignInForm.module.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function RegisterForm() {
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        setFormData((previousFormData) => ({
+            ...previousFormData,
+            [name]: value,
+        }));
+    }
+
     return (
         <div className={`${classes.formContainer}`}>
             <p className={`${classes.signUp}`}>Sign Up</p>
             <form action='#' method='post'>
                 <div className={`${classes.inputContainer}`}>
                     <p>First Name</p>
-                    <input type='text' id='firstName' name='first_name' maxLength={20} required/>
+                    <input type='text' id='firstName' name='first_name' onChange={handleChange} maxLength={20} required/>
                 </div>
                 <div className={`${classes.inputContainer}`}>
                     <p>Last Name</p>
-                    <input type='text' id='lastName' name='last_name' maxLength={20} required/>
+                    <input type='text' id='lastName' name='last_name' onChange={handleChange} maxLength={20} required/>
                 </div>
                 <div className={`${classes.inputContainer}`}>
                     <p>Email</p>
-                    <input type='email' id='email' name='user_email' required/>
+                    <input type='email' id='email' name='email' onChange={handleChange} required/>
                 </div>
                 <div className={`${classes.inputContainer}`}>
                     <p>Password</p>
-                    <input type='password' id='password' name='user_password' maxLength={128} minLength={5} required/>
+                    <input type='password' id='password' name='password' onChange={handleChange} maxLength={128} minLength={5} required/>
                 </div>
                 <button className={`${classes.submitButton}`} type='submit'>Create Account</button>
                 <p>Or <Link to='/login'>log in</Link>.</p>
