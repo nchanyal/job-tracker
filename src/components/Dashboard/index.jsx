128fd754e265c92df5a481e5c183fd2bd1ffde84
Nathnael Chanyalew <134805961+nchanyal@users.noreply.github.com>
2024-11-08 10:07:59 -0500

    Change the background color of Dashboard to gray

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,10 +1,11 @@
+import classes from "./Dashboard.module.css";
 import Navigation from "../Navigation";
 
 function Dashboard() {
     return (
-        <>
+        <div className={`${classes.dashboard}`}>
             <Navigation />
-        </>
+        </div>
     );
 }
 
